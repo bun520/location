b42a1c35df87a871ccd241087752ce812c4ffd4c
bun <wangbo.bun@gmail.com>
2022-02-17 14:19:37 +0800

    Initial Commit

---
+++ location/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  location
+//
+//  Created by wang on 2022/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
